c57462cba262d6c25be04b955a58ab8f95dccf25
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-06-03 18:14:29 +0530

    add function tu update the profile card

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const displayError = document.getElementById("errorBox");
+const profileCard = document.getElementById("profileCard");
 
 function showErrorMessage(message) {
     displayError.textContent = message;
     displayError.style.opacity = 1;
+}
+
+// Get all elements
+const profileImg = document.getElementById("profileImg");
+const profileName = document.getElementById("actualName");
+const userName = document.getElementById("userName");
+const profileFollowers = document.getElementById("profileFollower");
+const profileFollowing = document.getElementById("profileFollowing");
+const profileBio = document.getElementById("profileBio");
+const gitHubLink = document.getElementById("gitHubLink");
+
+function updateHTML(data, username) {
+    let { avatar_url, name, bio, login, followers, following } = data;
+
+    profileLink = `https://github.com/${username}`;
+
+    if (bio === null) {
+        bio = "";
+    }
+    // Update elemnts
+    profileImg.src = avatar_url;
+    profileName.textContent = `Name: ${name}`;
+    userName.textContent = `Username: ${login}`;
+    profileFollowers.textContent = `Followers: ${followers}`;
+    profileFollowing.textContent = `Following: ${following}`;
+    profileBio.textContent = `Bio: ${bio}`;
+    gitHubLink.href = profileLink;
 }
 
 async function getProfile(profileName) {
@@ -28,9 +56,7 @@ async function getProfile(profileName) {
         // Response converted to JSON
         const data = await response.json();
         console.log(data);
-
-        const { avatar_url, name, bio, login } = data;
-        console.log(avatar_url, name, bio, login);
+        updateHTML(data, profileName);
     } catch (error) {
         showErrorMessage(error.message);
         console.error(error);
@@ -47,6 +73,6 @@ form.addEventListener("submit", (event) => {
 
     // Hide the Error Box
     displayError.style.opacity = 0;
-
     getProfile(userInput);
+    profileCard.style.visibility = "visible";
 });
